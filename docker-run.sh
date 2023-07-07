f6f6b16a1a22de7a1bbb3093013d32f6b4a27700
iphearum <phearumgic@gmail.com>
2023-07-07 11:11:38 +0700

    upgrade api

--- docker-run.sh
+++ docker-run.sh
@@ -6,14 +6,14 @@ docker image build -t ${GPT}:tag . #5000
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-PORT=5000
-docker run -d -p ${PORT}:5000 ${GPT}:tag
+# PORT=5000
+# docker run -d -p ${PORT}:5000 ${GPT}:tag
 
-# COUNT=3
-# PORT=1337
-# for i in $(seq 1 $COUNT)
-# do
-#     docker run -d -p ${PORT}:1337 ${GPT}:tag
-#     PORT=$((PORT+1))
-# done
+COUNT=3
+PORT=1337
+for i in $(seq 1 $COUNT)
+do
+    docker run -d -p ${PORT}:1337 ${GPT}:tag
+    PORT=$((PORT+1))
+done
 # docker ps -al
