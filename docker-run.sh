d237b7f4f4efc76e4dc7b43c496ec079634dd36e
iphearum <phearumgic@gmail.com>
2023-07-07 16:57:28 +0700

    upgrade api

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 # docker build -t api_ai_chatbot:tag -f Dockerfile . #1337
 GPT="iphearum/gpt4all:tag"
 # docker push ${GPT}
-docker image build -t ${GPT} . #5000
+docker image build -t ${GPT} 1337:1337 -f Dockerfile . #5000
 
 # docker run -d -p 5000 api_ai_chatbot:tag
 docker stop $(docker ps -a -q)
